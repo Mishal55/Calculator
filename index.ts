e6e6cf8b683bb982787aa0eaa359c59c9d225758
MISHAL <160596015+Mishal55@users.noreply.github.com>
2024-06-07 13:10:08 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/ env node
+import inquirer from "inquirer"
+
+const ans = await inquirer.prompt([
+{   
+    message:"Enter your first number",
+    type:"number",
+    name: "Fisrtnumber",
+},
+
+{   
+    message:"Enter your second number",
+    type:"number",
+    name: "Secondnumber",
+},
+{
+    message:"Select one of the operators to perform action",
+    choices:["+","-","*" ,"/"],
+    type:"list",
+    name:"operator",
+},
+]);
+
+//Conditional statement
+
+if(ans.operator === "+"){
+    console.log(ans.Fisrtnumber + ans.Secondnumber);
+    
+}  else if(ans.operator === "-"){
+    console.log(ans.Fisrtnumber - ans.Secondnumber);
+    
+} else 
+if(ans.operator === "*"){
+    console.log(ans.Fisrtnumber *  ans.Secondnumber);
+    
+} else 
+if(ans.operator === "/"){
+    console.log(ans.Fisrtnumber / ans.Secondnumber);
+    
+} else{
+    console.log("Please select valid operators");
+    
+}
